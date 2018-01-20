17b4c931e4d5f43f30f26772b151795e26fa88f8
Marc Cataford <c.marcandre@gmail.com>
2018-01-19 22:04:08 -0500

    Docs

--- peerwise.py
+++ peerwise.py
@@ -12,6 +12,13 @@ class PeerwiseClient():
     self.session = requests.Session()
 
   def auth(self, uname, pwd, inst):
+    """
+    Gets the session cookies and creates the client's session object.
+    @param uname string Username
+    @param pwd string Password
+    @param inst string Institution shortcode, as defined by Peerwise (mcgill_ca for McGill)
+    @return List of course codes attached to the account.
+    """
     post_data = {
       "user": uname,
       "pass": pwd,
@@ -32,6 +39,12 @@ class PeerwiseClient():
     return course_codes
 
   def select_course(self, course_code):
+    """
+    Selects a course, necessary to access questions or any course-related data. Modifies the session.
+    @param course_code int Course identifier
+    @return The soup of the user dashboard page, if needed
+    """
+
     get_data = {
       "course_id": course_code
     }
@@ -39,6 +52,11 @@ class PeerwiseClient():
     return self.session.post(self.BASE_URL, data=get_data, headers={"User-Agent": "Mozilla/5.0"})
 
   def get_user_scores(self, course_code):
+    """
+    Gets the user's reputation and answer scores.
+    @param course_code int Course code.
+    @return Dict containing the scores.
+    """
 
     resp = self.select_course(course_code)
     soup = BeautifulSoup(resp.text, "lxml")
@@ -54,6 +72,12 @@ class PeerwiseClient():
     return scores
 
   def get_questions(self, course_code, mode):
+    """
+    Scrapes the questions from the selected listing (using mode)
+    @param course_code int Course code
+    @param mode string Selects unanswered, answered or own questions
+    @return List of question dicts.
+    """
 
     self.select_course(course_code)
 
@@ -117,10 +141,25 @@ class PeerwiseClient():
     return questions
 
   def get_own_questions(self, course_code):
+    """
+    Gets the user's own questions, uses get_question and mode=="own"
+    @param course_code int Course code.
+    @return List of dict containing the questions
+    """
     return self.get_questions(course_code, "own")
 
   def get_unanswered_questions(self, course_code):
+    """
+    Gets the user's unanswered questions, uses get_question and mode=="unanswered"
+    @param course_code int Course code.
+    @return List of dict containing the questions
+    """
     return self.get_questions(course_code, "unanswered")
 
   def get_answered_questions(self, course_code):
+    """
+    Gets the user's answered questions, uses get_question and mode=="answered"
+    @param course_code int Course code.
+    @return List of dict containing the questions
+    """
     return self.get_questions(course_code, "answered")
